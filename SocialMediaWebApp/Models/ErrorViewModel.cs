9dc95ecf10ae4a811f3252979df9d59f55543462
Finnley Grange <dev.finnleygrange@icloud.com>
2024-02-11 03:39:32 +0000

    Add project files.

---
+++ SocialMediaWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialMediaWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
